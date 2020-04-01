1d9ee3bb0e54fe31025d9df5fec830d5d858261d
Alex Li <tianyi7@ualberta.ca>
2020-03-31 22:26:37 -0600

    Add IL Tree interpreter
    Adding the basic post-order traversal interpretation mechanism of the
    IL tree. Right now the traversal is only done for each tree top in one
    basic block.
    
    This interpreter is implemented as one of the optimizations
    in order to directly interpret the IL before any other optimization
    happens.
    
    A nodeValueMap is used to save the results of nodes as they are processed for
    commoning. A symbol table is also kept for storing variables for load
    and store operations.
    
    Signed-off-by: Alex Li <tianyi7@ualberta.ca>

---
+++ compiler/optimizer/TreeInterpreter.cpp
@@ -0,0 +1,121 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2019 IBM Corp. and others
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at http://eclipse.org/legal/epl-2.0
+ * or the Apache License, Version 2.0 which accompanies this distribution
+ * and is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License, v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception [1] and GNU General Public
+ * License, version 2 with the OpenJDK Assembly Exception [2].
+ *
+ * [1] https://www.gnu.org/software/classpath/license.html
+ * [2] http://openjdk.java.net/legal/assembly-exception.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
+ *******************************************************************************/
+
+#include "optimizer/TreeInterpreter.hpp"
+#include <stddef.h>
+#include <stdint.h>
+#include "infra/forward_list.hpp"
+#include "codegen/CodeGenerator.hpp"
+#include "env/FrontEnd.hpp"
+#include "compile/Compilation.hpp"
+#include "compile/SymbolReferenceTable.hpp"
+#include "control/Options.hpp"
+#include "control/Options_inlines.hpp"
+#include "env/CompilerEnv.hpp"
+#include "env/IO.hpp"
+#include "env/StackMemoryRegion.hpp"
+#include "env/jittypes.h"
+#include "il/AutomaticSymbol.hpp"
+#include "il/Block.hpp"
+#include "il/DataTypes.hpp"
+#include "il/ILOpCodes.hpp"
+#include "il/ILOps.hpp"
+#include "il/MethodSymbol.hpp"
+#include "il/Node.hpp"
+#include "il/NodePool.hpp"
+#include "il/Node_inlines.hpp"
+#include "il/ResolvedMethodSymbol.hpp"
+#include "il/Symbol.hpp"
+#include "il/SymbolReference.hpp"
+#include "il/TreeTop.hpp"
+#include "il/TreeTop_inlines.hpp"
+#include "infra/Assert.hpp"
+#include "infra/BitVector.hpp"
+#include "infra/ILWalk.hpp"
+#include "infra/List.hpp"
+#include "optimizer/Optimization.hpp"
+#include "optimizer/Optimization_inlines.hpp"
+#include "optimizer/OptimizationManager.hpp"
+#include "optimizer/Optimizations.hpp"
+#include "optimizer/Optimizer.hpp"
+#include "optimizer/TransformUtil.hpp"
+#include "ras/Debug.hpp"
+
+
+TR::Optimization *
+TR::TreeInterpreter::create(TR::OptimizationManager *manager)
+   {
+   return new (manager->allocator()) TR::TreeInterpreter(manager);
+   }
+
+
+TR::TreeInterpreter::TreeInterpreter(TR::OptimizationManager *manager)
+   : TR::Optimization(manager),
+   nodeValueMap(std::less<ncount_t>(), comp()->trMemory()->currentStackRegion()),
+   symbolTable(std::less<TR::Symbol *>(), comp()->trMemory()->currentStackRegion())
+{
+}
+
+int32_t
+TR::TreeInterpreter::perform()
+{
+   TR::TreeTop *firstTree = comp()->getStartTree();
+   VALUE treeTopValue;
+   for ( TR::TreeTop * treeTop = firstTree; treeTop != NULL; treeTop = treeTop->getNextTreeTop()){
+      TR::Node *treeTopNode = treeTop->getNode();
+      traceMsg(comp(), "walking treeTop n%dn\n", treeTopNode->getGlobalIndex());
+      process(treeTopNode);
+      dumpNodeToValueMap();
+      if (treeTopNode->getOpCodeValue() == TR::treetop){
+         treeTopValue = nodeValueMap[treeTopNode->getChild(0)->getGlobalIndex()];
+         traceMsg(comp(), "treeTop n%dn VALUE(%s) = 0x%.8X\n",
+            treeTopNode->getGlobalIndex(), VALUETYPE_NAME[treeTopValue.type], treeTopValue.data);
+      }
+      traceMsg(comp(), "------------------------\n");
+   }
+   // interpretation finished, interrupt compiler
+   comp()->setInterpreterResult(treeTopValue.data.lconst);
+   comp()->failCompilation<TR::CompilationInterrupted>("IL interpretation finsihed");
+   return 1;
+}
+
+const char *
+TR::TreeInterpreter::optDetailString() const throw()
+   {
+   return "O^O TreeInterpreter: ";
+   }
+
+void
+TR::TreeInterpreter::process(TR::Node *node)
+{
+   if (nodeValueMap.find(node->getGlobalIndex()) != nodeValueMap.end()){
+      traceMsg(comp(), "\tprocessing node n%dn [%p], value found in map\n", node->getGlobalIndex(), node);
+      return;
+   }
+
+   int numChildren = node->getNumChildren();
+   for (int i = 0; i < numChildren; i++){
+      process(node->getChild(i));
+   }
+   traceMsg(comp(), "\tprocessing node n%dn [%p], evaluating\n", node->getGlobalIndex(), node);
+   performOp(node);
+   return;
+}
---
+++ compiler/optimizer/TreeInterpreter.hpp
@@ -0,0 +1,114 @@
+/*******************************************************************************
+ * Copyright (c) 2000, 2019 IBM Corp. and others
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at http://eclipse.org/legal/epl-2.0
+ * or the Apache License, Version 2.0 which accompanies this distribution
+ * and is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * This Source Code may also be made available under the following Secondary
+ * Licenses when the conditions for such availability set forth in the
+ * Eclipse Public License, v. 2.0 are satisfied: GNU General Public License,
+ * version 2 with the GNU Classpath Exception [1] and GNU General Public
+ * License, version 2 with the OpenJDK Assembly Exception [2].
+ *
+ * [1] https://www.gnu.org/software/classpath/license.html
+ * [2] http://openjdk.java.net/legal/assembly-exception.html
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0 OR GPL-2.0 WITH Classpath-exception-2.0 OR LicenseRef-GPL-2.0 WITH Assembly-exception
+ *******************************************************************************/
+
+#ifndef TR_TREEINTERPRETER_INCL
+#define TR_TREEINTERPRETER_INCL
+
+#include <stdint.h>
+#include "env/TRMemory.hpp"
+#include "infra/List.hpp"
+#include "optimizer/Optimization.hpp"
+#include "optimizer/Operation.hpp"
+#include <stack>
+#include <string>
+
+
+namespace TR { class Block; }
+namespace TR { class OptimizationManager; }
+namespace TR { class TreeTop; }
+
+namespace TR
+{
+
+
+class TreeInterpreter : public TR::Optimization
+{
+   public:
+   // aconst - address constant (zero value means NULL)
+   // iconst - integer constant (32-bit signed 2's complement)
+   // lconst - long integer constant (64-bit signed 2's complement)
+   // fconst - float constant (32-bit ieee fp)
+   // dconst - double constant (64-bit ieee fp)
+   // bconst - byte integer constant (8-bit signed 2's complement)
+   // sconst - short integer constant (16-bit signed 2's complement)
+   char VALUETYPE_NAME[9][10] = {
+      "NoType",
+      "Int8",
+      "Int16",
+      "Int32",
+      "Int64",
+      "Float",
+      "Double",
+      "Address",
+      "Boolean"
+   };
+   typedef enum {
+      NoType=0,
+      Int8,
+      Int16,
+      Int32,
+      Int64,
+      Float,
+      Double,
+      Address,
+      Boolean
+   } DATATYPE;
+   typedef union{
+      uintptrj_t  aconst;
+      int8_t      bconst;
+      int16_t     sconst;
+      int32_t     iconst;
+      int64_t     lconst;
+      float_t     fconst;
+      double_t    dconst;
+      bool        boolean;
+   } DATA;
+   
+   typedef struct {
+      DATATYPE type;
+      DATA data;
+   } VALUE;
+
+
+   typedef std::pair<ncount_t const, VALUE> NodeToValueMapEntry;
+   typedef TR::typed_allocator<NodeToValueMapEntry, TR::Region&> NodeToValueMapAlloc;
+   typedef std::map<ncount_t, VALUE, std::less<ncount_t>, NodeToValueMapAlloc> NodeToValueMap;
+   NodeToValueMap nodeValueMap;
+
+   typedef std::pair<TR::Symbol * const, VALUE> SymbolTableEntry;
+   typedef TR::typed_allocator<SymbolTableEntry, TR::Region&> SymbolTableAlloc;
+   typedef std::map<TR::Symbol *, VALUE, std::less<TR::Symbol *>, SymbolTableAlloc> SymbolTable;
+   SymbolTable symbolTable;
+
+   TreeInterpreter(TR::OptimizationManager *manager);
+   static TR::Optimization *create(TR::OptimizationManager *manager);
+   virtual int32_t perform();
+   virtual const char * optDetailString() const throw();
+
+   private:
+   void process(TR::Node *node);
+
+   // operations
+   void performOp(TR::Node * node);
+};
+}
+
+#endif
